35af17e77df63d1858a82bdd7f6bf283eef1bd96
akshays236 <akshays23623@gmail.com>
2025-06-11 14:37:11 +0530

    Add app urls in project urls

--- echosorter_project/urls.py
+++ echosorter_project/urls.py
@@ -16,8 +16,15 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
+
+app_name = "spotify_integration"
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("spotify/", include("spotify_integration.urls")),
+    path("auth/", views.auth_spotify, name="auth_spotify"),
+    path("callback/", views.spotify_callback, name="spotify_callback"),
+    path("liked-songs/", views.liked_songs, name="liked_songs"),
 ]
