8e59a2ad9e9fb268d1a00d3c841232a40af1520f
Akshay S <akshays23623@gmail.com>
2025-07-09 13:35:14 +0530

    Add url patterns for playlist feature

--- spotify_integration/urls.py
+++ spotify_integration/urls.py
@@ -6,10 +6,9 @@ from . import views  # Import views from the current app
 app_name = "spotify_integration"  # This is important for namespacing URLs
 
 urlpatterns = [
-    path(
-        "auth/", views.auth_spotify, name="auth_spotify"
-    ),  # This was likely your original auth path
+    path("", views.home, name="home"),
+    path("auth/", views.auth_spotify, name="auth_spotify"),
     path("callback/", views.spotify_callback, name="spotify_callback"),
     path("liked_songs/", views.liked_songs, name="liked_songs"),
-    # Add other paths specific to your spotify_integration app here if needed
+    path("create_playlist/", views.create_playlist, name="create_playlist"),
 ]
